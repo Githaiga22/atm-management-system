7ae6f642b8e4cdcea332b7ff2cd276a6be20eb63
allkamau <allangithaiga5@gmail.com>
2024-10-29 10:38:27 +0300

    update header.h

--- atm-system/src/header.h
+++ atm-system/src/header.h
@@ -1,15 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-struct Date
-{
+struct Date {
     int month, day, year;
 };
 
-// all fields for each record of an account
-struct Record
-{
+// All fields for each record of an account
+struct Record {
     int id;
     int userId;
     char name[100];
@@ -22,19 +23,27 @@ struct Record
     struct Date withdraw;
 };
 
-struct User
-{
+struct User {
     int id;
     char name[50];
     char password[50];
 };
 
-// authentication functions
+// Authentication functions
 void loginMenu(char a[50], char pass[50]);
 void registerMenu(char a[50], char pass[50]);
 const char *getPassword(struct User u);
 
-// system function
+// System functions
 void createNewAcc(struct User u);
 void mainMenu(struct User u);
 void checkAllAccounts(struct User u);
+void updateAccountInformation(struct User u);
+void checkAccountDetails(struct User u);
+void makeTransaction(struct User u);
+void removeAccount(struct User u);
+void transferOwnership(struct User u);
+void handleFailedLogin(struct User *u);
+void handleFailedRegistration(char a[50], char pass[50]);
+
+#endif // HEADER_H
